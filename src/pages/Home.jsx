d4e28c250e6d911a583c54ccd3c6823d93550b05
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2022-12-07 20:21:36 +0530

    added cards and mapped it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
-import { Box} from "@chakra-ui/react"
+import {Flex,SimpleGrid} from "@chakra-ui/react"
 import {Navbar} from "../components/Navbar/Navbar"
+import Cards from '../components/cards/cards'
+
+const Names =[
+  'joseph',
+  'john',
+  'ezbon',
+  'aadesh',
+  'vishak',
+  'nikil',
+  'nived',
+  'ebin',
+  'jithin'
+]
 
 
 export const Home = () => {
   return (
-   <Box bg={{base:"red",md:"blue",lg:"white"}} height="100vh">
+    <Flex flexDirection={'column'}>
    <Navbar/>
-
-   </Box>
+   <SimpleGrid columns={{lg:4,md:3,base:1}} spacingX='40px' spacingY='20px'paddingLeft={'150px'} paddingRight={'150px'}>
+   {/*<Cards Name='HEllo world'/>
+   <Cards Name='HEllo world'/>
+   <Cards Name='HEllo world'/>
+   <Cards Name='HEllo world'/>
+   <Cards Name='HEllo world'/>
+   <Cards Name='HEllo world'/>
+   <Cards Name='HEllo world'/>
+  <Cards Name='HEllo world'/>*/}
+   {Names.map((item,index)=>{
+    return(
+      <Cards Name={item} key={index} />
+    )
+   })}
+   </SimpleGrid>
+   </Flex>
   )
 }
 
