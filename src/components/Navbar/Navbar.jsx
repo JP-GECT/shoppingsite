e570c8d6f2f659614a24bca242d513718c04e359
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2022-12-08 19:47:07 +0530

    added style to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import {Heading,Flex,Input,InputLeftAddon,InputGroup,InputRightElement} from "@chakra-ui/react"
+import {Heading,Flex,Input,InputLeftAddon,InputGroup,InputRightElement,Text} from "@chakra-ui/react"
 import { SearchIcon } from '@chakra-ui/icons'
 
 export const Navbar = ()=>{
     return (
-       <Flex bg="#2ee6e3" h="100px" align="center" justifyContent="space-between" p="6">
-           <Heading color="white">jMART</Heading>
+       <Flex bg="#fff" h="100px" align="center" justifyContent="space-around" p="6">
+           <Heading color="green">jMART</Heading>
            <InputGroup size='sm' width='500px'>
     <InputLeftAddon children='search' />
-    <Input focusBorderColor='pink' placeholder='wanna buy something' color="black" borderColor="white"   _placeholder={{ opacity: 0.5, color: 'gray.500' }}/>
-    <InputRightElement borderColor="black" children={<SearchIcon color='white' />} />
+    <Input focusBorderColor='pink' placeholder='wanna buy something' color="black" borderColor="green"   _placeholder={{ opacity: 0.5, color: 'gray.500' }}/>
+    <InputRightElement borderColor="black" children={<SearchIcon color='green' />} />
   </InputGroup>
+  <Flex>
+    <Flex flexDirection={'column'} color='green'>
+        <Text>NO of items</Text>
+        <Text>sub total:0</Text>
+    </Flex>
            <Heading>🛒</Heading>
+    </Flex>
        </Flex>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Names =[
 
 export const Home = () => {
   return (
-    <Flex flexDirection={'column'}>
+    <Flex flexDirection={'column'} background={'#f5f5f5'}>
    <Navbar/>
    <SimpleGrid columns={{lg:4,md:3,base:1}} spacingX='40px' spacingY='20px'paddingLeft={'150px'} paddingRight={'150px'}>
    {/*<Cards Name='HEllo world'/>
